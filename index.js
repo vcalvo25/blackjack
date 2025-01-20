9d8931e4c3fc0689c62002d3102c294314f3a72e
vcalvo25 <victoria.calvo1013@hotmail.com>
2025-01-19 23:08:48 -0500

    delete old jpg, add more styles and javascript

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 
-let firstCard = 10;
+let firstCard = 11;
 let secondCard = 10;
 let cardSum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-console.log(cardSum)
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-if (cardSum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (cardSum === 21) {
-    message = "Congrats, you've won Blackjack!"
-    hasBlackJack = true
-}else {
-    message = "Sorry, you're out of the game!"
-    isAlive = false
+function startGame() {
+    renderGame()
 }
 
-console.log(isAlive)
-console.log(message)
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: "  + cardSum
+    if (cardSum <= 20) {
+        message = "Do you want to draw a new card? "
+    } else if (cardSum === 21) {
+        message = "Congrats, you've got Blackjack!"
+        hasBlackJack = true
+    }else {
+        message = "Sorry, you're out of the game!"
+        isAlive = false
+    }
+ 
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+
+    let addCard = 7
+
+    cardSum += addCard
+    renderGame()
+ 
+}
 
 
